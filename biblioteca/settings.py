4406dbd4f9d777d61a326f0997b4a9970f32fc96
Cairo de Paula Cunha Gomes <167835379+CairodePaula@users.noreply.github.com>
2026-09-16 21:58:31 -0300

    fix: troca banco de PostgreSQL para SQLite

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -51,12 +51,8 @@ ASGI_APPLICATION = "biblioteca.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "biblioteca_db"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
